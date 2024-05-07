ce7f6a48fc1d128e79eb5ccc70493cac8eceb831
MD SIFAT KHAN <sifatkhann229@gmail.com>
2024-05-07 23:31:48 +0800

    function added

--- rsa.py
+++ rsa.py
@@ -4,3 +4,15 @@ q = int(input('Inplut a prime number as q: '))
 n = p*q
 phi_n = (p-1) * (q-1)
 
+def check_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num**0.5+1):
+        if num % i == 0:
+            return False
+    return True
+
+if check_prime(p):
+    print(f"{p} is a prime number")
+else:
+    print(f"{p} is not a prime number")
