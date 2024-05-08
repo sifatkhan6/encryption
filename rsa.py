b901085575056f426e1fa768aefeefe8ada2b908
MD SIFAT KHAN <sifatkhann229@gmail.com>
2024-05-08 17:51:05 +0800

    trying to make dynamic

--- rsa.py
+++ rsa.py
@@ -1,18 +1,26 @@
-p = int(input('Inplut a prime number as p: '))
-q = int(input('Inplut a prime number as q: '))
+p = int(input('Input a prime number as p: '))
+q = int(input('Input a prime number as q: '))
 
 n = p*q
 phi_n = (p-1) * (q-1)
 
 def check_prime(num):
     if num <= 1:
-        return False
-    for i in range(2, num**0.5+1):
+        print(f'{num} is not a prime number')
+        new_num = int(input(f'Input a prime number replace of {num}: '))
+        num = new_num
+        return num
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            return False
-    return True
+            print(f'{num} is not a prime number')
+            new_num = int(input(f'Input a prime number replace of {num}: '))
+            return new_num
+        break
+    else:
+        print(f'{num} is a prime number')
 
-if check_prime(p):
-    print(f"{p} is a prime number")
-else:
-    print(f"{p} is not a prime number")
+new_input = check_prime()
+
+check_prime(p)
+check_prime(q)
+
