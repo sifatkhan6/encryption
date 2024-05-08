d87796e5c6cf0413f23ba2c3f70ba73c3798dc27
MD SIFAT KHAN <sifatkhann229@gmail.com>
2024-05-09 04:40:44 +0800

    rsa implemented

--- rsa.py
+++ rsa.py
@@ -1,4 +1,3 @@
-import random
 import math
 
 p = int(input('Input a prime number as p: '))
@@ -35,52 +34,27 @@ phi_n = (p-1) * (q-1)
 
 print(f'n = {n} and phi_n = {phi_n}')
 
-e = random.randint(2, phi_n - 1)
+for i in range(2, phi_n):
+    if math.gcd(i, phi_n) == 1:
+        e = i
+        break
 
-def compute_e():
-    global e
-    while math.gcd(e, phi_n != 1):
-        e = random.randint(2, phi_n - 1)
-        print(f'public key is = {e}')
-        return e
-
-def modular_inverse(e, phi):
-    d = 0
-    x1 = 0
-    x2 = 1
-    y1 = 1
-    temp_phi = phi
-
-    while e > 0:
-        temp1 = temp_phi // e
-        temp2 = temp_phi - temp1 * e
-        temp_phi = e
-        e = temp2
-
-        x = x2 - temp1 * x1
-        y = d - temp1 * y1
-
-        x2 = x1
-        x1 = x
-        d = y1
-        y1 = y
-
-    if temp_phi == 1:
-        return d + phi
-
-d = modular_inverse(e, phi_n)
+j = 0
+while True:
+    if (j * e) % phi_n == 1:
+        d = j
+        break
+    j += 1
 
 print(f'private key is = {d}')
 print(f'public key is = {e}')
 
-plain_text = int(input('Input your integer message: '))
+msg = int(input('Input your integer message: '))
 
 #encryption
-C = pow(plain_text, e)
-C = math.fmod(C, n)
+C = (msg ** e) % n
 print(f'Encrypted message is : {C}')
 
 #decryption
-M = pow(C, d)
-M = math.fmod(M, n)
+M = (C ** d) % n
 print(f'Original message is : {M}')
