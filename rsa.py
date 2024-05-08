aeba1a88e4eae631384b59b304e022525909bfb0
MD SIFAT KHAN <sifatkhann229@gmail.com>
2024-05-09 04:08:40 +0800

    key pair funcrion

--- rsa.py
+++ rsa.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 p = int(input('Input a prime number as p: '))
 q = int(input('Input a prime number as q: '))
 
@@ -31,3 +34,53 @@ n = p * q
 phi_n = (p-1) * (q-1)
 
 print(f'n = {n} and phi_n = {phi_n}')
+
+e = random.randint(2, phi_n - 1)
+
+def compute_e():
+    global e
+    while math.gcd(e, phi_n != 1):
+        e = random.randint(2, phi_n - 1)
+        print(f'public key is = {e}')
+        return e
+
+def modular_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+
+    while e > 0:
+        temp1 = temp_phi // e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
+
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_phi == 1:
+        return d + phi
+
+d = modular_inverse(e, phi_n)
+
+print(f'private key is = {d}')
+print(f'public key is = {e}')
+
+plain_text = int(input('Input your integer message: '))
+
+#encryption
+C = pow(plain_text, e)
+C = math.fmod(C, n)
+print(f'Encrypted message is : {C}')
+
+#decryption
+M = pow(C, d)
+M = math.fmod(M, n)
+print(f'Original message is : {M}')
